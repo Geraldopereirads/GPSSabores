3554a71ba4b4fa21825100ab111453a182817e0e
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-10-17 16:09:35 -0300

    feat:(infrastructure): adds DataAccess, migrations, and infrastructure configuration

---
+++ src/Backend/GPSSabores.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using GPSSabores.Domain.Repositories;
+using GPSSabores.Domain.Repositories.User;
+using GPSSabores.Infrastructure.DataAccess;
+using GPSSabores.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GPSSabores.Infrastructure;
+public static class DependencyInjection
+{
+    public static void AddInfrastruture(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext_SqlServer(services, configuration);
+        AddRepositories(services);
+    }
+
+    private static void AddDbContext_SqlServer(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("ConnectionSQLServer");
+        services.AddDbContext<GpsSaboresDbContext>(dbContextOptions =>
+        {
+            dbContextOptions.UseSqlServer(connectionString);
+        });
+    }
+    private static void AddRepositories(IServiceCollection services)
+    {
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+    }
+}
