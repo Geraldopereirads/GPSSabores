5759e2a0b09254302f4a678e07daf3c265cec4c8
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-10-17 16:05:02 -0300

    feat(application): adds services, use cases, and dependency injection

---
+++ src/Backend/GPSSabores.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GPSSabores.Application.Services.Cryptography;
+
+public class PasswordEncripter
+{
+    private readonly string _additionalKey;
+    public PasswordEncripter(string additionalKey) => _additionalKey = additionalKey;
+
+    public string Encrypt(string password)
+    {
+
+
+        var newPassword = $"{password}{_additionalKey}";
+        var bytes = Encoding
+            .UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+
+        return StringBytes(hashBytes);
+    }
+
+    private static string StringBytes(byte[] bytes)
+    {
+        var db = new StringBuilder();
+        foreach (byte b in bytes)
+        {
+            var hex = b.ToString("x2");
+            db.Append(hex);
+        }
+        return db.ToString();
+    }
+
+}
+
