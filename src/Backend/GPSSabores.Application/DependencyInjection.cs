548b15aefdb5a3bf5bc94fcc3f741d68b5039840
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-10-17 16:19:36 -0300

    feat:(application): adds configuration and design of the Application layer

---
+++ src/Backend/GPSSabores.Application/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using GPSSabores.Application.Services.AutoMapper;
+using GPSSabores.Application.Services.Cryptography;
+using GPSSabores.Application.UseCases.User.Register;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GPSSabores.Application;
+public static class DependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        AddPasswordEncrypter(services, configuration);
+        AddAutoMapper(services);
+        AddUseCases(services);
+
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        var autoMapper = new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapping());
+        }).CreateMapper();
+        services.AddScoped(option => autoMapper);
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserCase, RegisterUserCase>();
+    }
+
+    private static void AddPasswordEncrypter(IServiceCollection services, IConfiguration configuration)
+    {
+        var additionalKey = configuration.GetValue<String>("Settings:Password:AdditionalKey");
+        services.AddScoped(option => new PasswordEncripter(additionalKey!));
+    }
+}
