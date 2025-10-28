442049f0f57046b81dacace0334b321ec1601689
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-10-27 22:24:13 -0300

    test(user-registration): add unit tests for user registration use case

--- tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -7,5 +7,11 @@ public class UserReadOnlyRepositoryBuilder
     private readonly Mock<IUserReadOnlyRepository> _repository;
 
     public UserReadOnlyRepositoryBuilder() => _repository = new Mock<IUserReadOnlyRepository>();
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _repository.Setup(repository => repository.ExistActiveUserWithEmail(email)).ReturnsAsync(true);
+    }
+
     public IUserReadOnlyRepository Build() => _repository.Object;
 }
